034ee6e89b52e2fba0f4b91aa9a32d67f2cda4e9
wasimabass <wasimabass@gmail.com>
2021-05-17 12:42:17 +0500

    new index

--- js/main.js
+++ js/main.js
@@ -618,73 +618,131 @@ Author : Repair Mobile
 
 
 
-jQuery(document).ready(function(){
 
-var current_fs, next_fs, previous_fs; //fieldsets
-var opacity;
 
-jQuery(".next").click(function(){
+$(document).ready(function () {
+    
+    var current_fs, next_fs, previous_fs; //fieldsets
+    var left, opacity, scale; //fieldset properties which we will animate
+    var animating; //flag to prevent quick multi-click glitches
 
-current_fs = jQuery(this).parent();
-next_fs = jQuery(this).parent().next();
+    $(".next").click(function () {
+    
+        $('#reg_form').validate({
+            rules: {    
+                checkoutFullName: {
+                    required: true
+                },
+                checkoutStore: {
+                    required: true
+                },
+                checkoutWorkEamil: {
+                    required: true
+                }
+            },
+            messages: {
+                checkoutFullName: "Please specify a file",
+                checkoutStore: "Title is required",
+                checkoutWorkEamil: "Description is required"
+            }
+        });
 
-//Add Class Active
-jQuery("#progressbar li").eq(jQuery("fieldset").index(next_fs)).addClass("active");
+        if (!jQuery('#reg_form').valid()) {
+         alert('no no');
+           return false;
+        }
+        
+        if (animating) return false;
+        animating = true;
 
-//show the next fieldset
-next_fs.show();
-//hide the current fieldset with style
-current_fs.animate({opacity: 0}, {
-step: function(now) {
-// for making fielset appear animation
-opacity = 1 - now;
+        // current_fs = $(this).parent();              //fieldset
+        // next_fs = $(this).parent().next();          //next fieldset
 
-current_fs.css({
-    'display': 'none',
-    'position': 'relative'
-});
-    next_fs.css({'opacity': opacity});
-},duration: 600
-    });
-});
+        current_fs = jQuery(this).parents('fieldset');
+next_fs = jQuery(this).parents('fieldset').next();
 
-jQuery(".previous").click(function(){
 
-current_fs = jQuery(this).parent();
-previous_fs = jQuery(this).parent().prev();
+        //activate next step on progressbar using the index of next_fs
+        $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
 
-//Remove class active
-jQuery("#progressbar li").eq(jQuery("fieldset").index(current_fs)).removeClass("active");
-
-//show the previous fieldset
-previous_fs.show();
-
-//hide the current fieldset with style
-current_fs.animate({opacity: 0}, {
-step: function(now) {
-    // for making fielset appear animation
-    opacity = 1 - now;
-
-    current_fs.css({
-    'display': 'none',
-    'position': 'relative'
-    });
-    previous_fs.css({'opacity': opacity});
-        },duration: 600
+        //show the next fieldset
+        next_fs.show();
+        //hide the current fieldset with style
+        current_fs.animate({
+            opacity: 0
+        }, {
+            step: function (now, mx) {
+                //as the opacity of current_fs reduces to 0 - stored in "now"
+                //1. scale current_fs down to 80%
+                scale = 1 - (1 - now) * 0.2;
+                //2. bring next_fs from the right(50%)
+                left = (now * 50) + "%";
+                //3. increase opacity of next_fs to 1 as it moves in
+                opacity = 1 - now;
+                current_fs.css({
+                    'transform': 'scale(' + scale + ')'
+                });
+                next_fs.css({
+                    'left': left,
+                    'opacity': opacity
+                });
+            },
+            duration: 800,
+            complete: function () {
+                current_fs.hide();
+                animating = false;
+            },
+            //this comes from the custom easing plugin
+            easing: 'easeInOutBack'
         });
     });
 
-    jQuery('.radio-group .radio').click(function(){
-        jQuery(this).parent().find('.radio').removeClass('selected');
-        jQuery(this).addClass('selected');
+    $(".previous").click(function () {
+        if (animating) return false;
+        animating = true;
+
+        current_fs = jQuery(this).parents('fieldset');
+previous_fs = jQuery(this).parents('fieldset').prev();
+
+        //de-activate current step on progressbar
+        $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+
+        //show the previous fieldset
+        previous_fs.show();
+        //hide the current fieldset with style
+        current_fs.animate({
+            opacity: 0
+        }, {
+            step: function (now, mx) {
+                //as the opacity of current_fs reduces to 0 - stored in "now"
+                //1. scale previous_fs from 80% to 100%
+                scale = 0.8 + (1 - now) * 0.2;
+                //2. take current_fs to the right(50%) - from 0%
+                left = ((1 - now) * 50) + "%";
+                //3. increase opacity of previous_fs to 1 as it moves in
+                opacity = 1 - now;
+                current_fs.css({
+                    'left': left
+                });
+                previous_fs.css({
+                    'transform': 'scale(' + scale + ')',
+                    'opacity': opacity
+                });
+            },
+            duration: 800,
+            complete: function () {
+                current_fs.hide();
+                animating = false;
+            },
+            //this comes from the custom easing plugin
+            easing: 'easeInOutBack'
+        });
     });
 
-    jQuery(".submit").click(function(){
+
+    $(".submit").click(function () {
         return false;
     });
-
 });
-
-
 
 
